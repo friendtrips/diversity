dff243d257c189d1517ccb9eb0afaee00e2cb9e6
Brooke <brooke.snellings@gmail.com>
2019-09-21 13:41:05 -0500

    add react-router-dom

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import FlightDetails from './FlightDetails';
 import LandingPage from './LandingPage';
 import FlightList from './FlightList';
@@ -7,15 +9,18 @@ import BookTrip from './BookTrip';
 import PaymentPage from './PaymentPage';
 import Confirmation from './Confirmation';
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return <h1>Hello, world!</h1>;
+    return (
+      <Router>
+        <h1>Hello, world!</h1>
+        <Route path="/" exact component={LandingPage} />
+      </Router>
+    )
   }
 }
-
-export default App;
