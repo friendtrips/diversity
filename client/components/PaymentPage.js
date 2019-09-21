6864f96751860b840772416cf8c32ea1db260a0d
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-09-21 11:18:10 -0500

    components: BookTrip, PaymentPage, Confirmation

---
+++ client/components/PaymentPage.js
@@ -0,0 +1,15 @@
+//Summary: The PaymentsPage will provide a summation of the flight info for each user and whether they have paid or not which will
+//be indicated by a green check (or something) for paid, and a red (something) to indicate they have not paid
+//States Passed Down From App: User Names, Flight Info for Each User, Paid or Not Paid (0=No, 1=Yes)
+import React from 'react';
+
+const PaymentsPage = (props) => {
+    return (
+        <div>
+            <text>Here are Confrimations</text>
+        </div>
+    )
+}
+
+export default PaymentsPage 
+
