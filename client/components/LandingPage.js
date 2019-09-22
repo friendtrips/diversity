9ad61b48560cd94024573d0171a6b82c2041f516
Brooke <brooke.snellings@gmail.com>
2019-09-22 12:28:42 -0500

    MVP Landing

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -16,13 +16,17 @@ const LandingPage = (props) => {
           left: '-20px'
         }}>
           <img src={require("../../dist/templogo.png")} width="200" height="97" /></div>
-        <div style={{ fontFamily: 'Roboto', color: 'white', textShadow: '2px 2px black', fontSize: '30px', position: 'absolute', top: '45%', left: '50%', transform: 'translate(-50%, -50%' }}>Here is some text about our app, inviting you to use it and telling you how and why you should.
+        <div style={{ fontFamily: 'Roboto', color: 'white', textShadow: '2px 2px black', fontSize: '30px', position: 'absolute', top: '45%', left: '50%', transform: 'translate(-50%, -50%' }}>
+          <h1 style={{ marginTop: '140px' }}>Travel, together.</h1>
+          <h3>Here is some text about our app, inviting you to use it and telling you how and why you should.</h3>
         </div>
-        <div style={{ marginTop: "50px", margin: '0 auto', position: 'absolute', top: '60%', left: '50%' }}>
-          <Button onClick={() => { props.changePage(1) }} variant="contained">Plan your next friend trip</Button>
+        <div style={{ marginTop: "150px", margin: '0 auto', position: 'absolute', top: '80%', left: '50%' }}>
+          <Button onClick={() => {
+            props.changePage(1)
+          }} variant="contained">Plan your next friend trip</Button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
